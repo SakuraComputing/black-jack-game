bdd87b8876b4caba28de21388920750487d5442a
Ewan Bauld <ewan.bauld@gmail.com>
2025-03-23 21:50:27 +0000

    Add react-countup

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import CountUp from 'react-countup';
+
 import * as styles from '../../pages/styles/Counter.module.css';
 
 const Counter = ({ count }) => {
-  return <div className={styles.counter}>{count}</div>;
+  return (
+    <div className={styles.counter}>
+      <CountUp end={count} />
+    </div>
+  );
 };
 
 export default Counter;
